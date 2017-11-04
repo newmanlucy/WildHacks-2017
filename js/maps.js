2dc9b5bf92d0e4e50cf42e5614be05a688c96357
Lucy Newman <newmanlucy@uchicago.edu>
2017-11-04 13:23:26 -0500

    styling and added place search

--- js/maps.js
+++ js/maps.js
@@ -8,3 +8,46 @@ function updateWeather(checkbox) {
     alert("b");
   }
 }
+
+function showPlaces(neigborhoodLoc, placeType) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: neigborhoodLoc,
+    zoom: 15
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+  console.log(service);
+
+  var request = {
+    location: neigborhoodLoc,
+    radius: '500',
+    type: [placeType]
+  };
+
+  console.log(request);
+  service.nearbySearch(request, callback);
+}
+
+
+function callback(results, status) {
+  console.log("callback");
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
